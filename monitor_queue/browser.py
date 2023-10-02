dc94167f283dfbb2a32389b49480c8f21f69bfb0
Your Name <you@example.com>
2023-10-02 10:07:45 +0700

    no comments

--- monitor_queue/browser.py
+++ monitor_queue/browser.py
@@ -5,6 +5,8 @@ import csv
 import base64
 import re
 import datetime
+import sys
+
 
 from monitor_queue.parser import get_days, get_times
 from monitor_queue.funcs import asleep, dt_now, aprint, aprint2, ainit,make_sckeernshot, h_now
@@ -20,14 +22,35 @@ DELAY_ANALYSE=5
 DELAY_WAIT=1000
 DELAY_BAD_CAPTCHA=120
 
+WORK_START=2
+WORK_END=11
 
-FAST_START=3
-FAST_END=8
-VERYFAST_HOUR=4
+FAST_START=2
+FAST_END=9
 
-DELAY_WAIT_VERYFAST=25
-DELAY_WAIT_FAST=125
-DELAY_WAIT_NORMAL=1000
+VERYFAST0_HOUR=3
+#VERYFAST0_MIN_START=50
+#VERYFAST0_MIN_END=60
+
+VERYFAST1_HOUR=4
+#VERYFAST1_MIN_START=0
+#VERYFAST1_MIN_END=0
+
+VERYFAST2_HOUR=6
+#VERYFAST2_MIN_START=0
+#VERYFAST2_MIN_END=0
+
+VERYFAST3_HOUR=7
+#VERYFAST3_MIN_START=0
+#VERYFAST3_MIN_END=0
+
+
+DELAY_WAIT_VERYFAST=5
+DELAY_WAIT_FAST=25
+DELAY_WAIT_NORMAL=1500
+
+BROWSER_LOAD_TIMEOUT = 30  # Adjust this value as needed
+
 
 aprint("Init browser...")
 #chrome_options = Options()
@@ -86,35 +109,51 @@ def wait_captcha(driver):
         if len(capthca_answer_text)>0:
             aprint("captcha введена")
             aprint(capthca_answer_text)
-            return True
+            return capthca_answer_text
         aprint("captcha не введена")
         asleep(1)
 
     aprint("captcha не введена в течение минуты")
-    return False
+    return ""
 
 def open_browser(query,id):
     #query = f"about:blank"
     ainit(query,id)
-    aprint(query)
-    
-    print("Browser navigate", query, id)
-    driver.get(query)  # Открытие страницы Google
-    aprint("Мониторинг ситуации")
 
     working_days=[]
+    init=False
 
     while True:
-        aprint(dt_now())
+
         hnow=h_now()
 
-        if (hnow==VERYFAST_HOUR):
+        if((hnow<WORK_START) or (hnow>WORK_END)):
+            asleep(321)
+            continue
+        aprint(dt_now())
+
+
+        if ((hnow==VERYFAST0_HOUR) or (hnow==VERYFAST1_HOUR) or (hnow==VERYFAST2_HOUR) or (hnow==VERYFAST3_HOUR)):
             DELAY_WAIT=DELAY_WAIT_VERYFAST
         elif (hnow>=FAST_START) and (hnow<FAST_END):
             DELAY_WAIT=DELAY_WAIT_FAST
         else:
             DELAY_WAIT=DELAY_WAIT_NORMAL
     
+        if(init==False):
+            init=True
+            aprint(query)
+    
+            print("Browser navigate", query, id)
+            driver.get(query)  # Открытие страницы Google
+            aprint("Мониторинг ситуации")
+
+        try:
+            element_present = WebDriverWait(driver, BROWSER_LOAD_TIMEOUT).until(
+                EC.presence_of_element_located((By.TAG_NAME, 'body'))
+            )
+        except TimeoutException:
+            print("Timed out waiting for page to load")
 
         html_code=driver.page_source
         response_text=html_code
@@ -122,9 +161,16 @@ def open_browser(query,id):
 
 
         # aprint(response_text)
+        current_url=driver.current_url
 
 
-        if text_wrong in response_text:
+        if "blocked" in current_url:
+            aprint("BLOCKED")
+            make_sckeernshot(driver)
+            #driver.refresh()
+            #aprint("refresh")
+            sys.exit(1)
+        elif text_wrong in response_text:
             aprint("Something went wrong")
             make_sckeernshot(driver)
             #driver.refresh()
@@ -145,23 +191,29 @@ def open_browser(query,id):
         elif text_fill in response_text:
             aprint("Вводим капчу")
             
-            if wait_captcha(driver):
+            captcha=wait_captcha(driver)
+            if (re.match(r'^\d{6}$', captcha)):
                 asleep(3)
                 aprint("Нажимаем")
                 do_send_form(driver)
                 asleep(DELAY_AFTER_ACTION)
-            else:
+            elif (captcha==""):
+                aprint("no captcha result, wait and refresh")
                 asleep(DELAY_BAD_CAPTCHA)
                 driver.refresh()
                 aprint("refresh")
                 asleep(DELAY_AFTER_ACTION)
-
+            else:
+                aprint("wrong captcha, just refresh")
+                driver.refresh()
+                aprint("refresh")
+                asleep(DELAY_AFTER_ACTION)                
 
 
 
         elif text_notime in response_text:
             aprint("Нет свободного времени => обновляем")
-            make_sckeernshot(driver)
+            #make_sckeernshot(driver)
             asleep(DELAY_WAIT)
             driver.refresh()
             aprint("refresh")
@@ -169,7 +221,7 @@ def open_browser(query,id):
 
         elif text_choose in response_text:
             aprint("choose_time -> action")
-            make_sckeernshot(driver)
+            #make_sckeernshot(driver)
 
             working_days_before=working_days
             working_days=get_days(soup)
--- start_bg.sh
+++ start_bg.sh
@@ -1,30 +1,56 @@
 #export DISPLAY=:0
 
-#phuket
+#./inet_mobile.sh
+
+#BANGKOK
+
+nohup python3 -m monitor_queue "https://bangkok.kdmid.ru/queue/orderinfo.aspx?id=67053&cd=df568b77&ems=216E47E1" 67053 >>logs/bangkok_pasport5.txt 2>>logs/bangkok_pasport5.err  &
+sleep 6
+nohup python3 -m monitor_queue "https://bangkok.kdmid.ru/queue/orderinfo.aspx?id=67054&cd=bc214c88&ems=B7F84791" 67054 >>logs/bangkok_pasport10.txt 2>>logs/bangkok_pasport10.err  &
+sleep 6
+
+#https://bangkok.kdmid.ru/queue/orderinfo.aspx?id=67053&cd=df568b77&ems=216E47E1
+#https://bangkok.kdmid.ru/queue/orderinfo.aspx?id=67054&cd=bc214c88&ems=B7F84791
+#https://bangkok.kdmid.ru/queue/orderinfo.aspx?id=67055&cd=21ec3ebd&ems=568E4C2B
+#https://bangkok.kdmid.ru/queue/orderinfo.aspx?id=67050&cd=a6cd526a&ems=6041431B
+#https://bangkok.kdmid.ru/queue/orderinfo.aspx?id=67051&cd=8e59d8e1&ems=BCEF4C44
+#https://bangkok.kdmid.ru/queue/orderinfo.aspx?id=67052&cd=25f0d6b0&ems=CF8F4FDE
+
+nohup python3 -m monitor_queue "https://bangkok.kdmid.ru/queue/orderinfo.aspx?id=67470&cd=3a41a16c&ems=B52C4132" 67470 >>logs/bangkok_citizenship.txt 2>>logs/bangkok_citizenship.err  &
+sleep 6
+nohup python3 -m monitor_queue "https://bangkok.kdmid.ru/queue/orderinfo.aspx?id=67469&cd=2043f63c&ems=A4D642C0" 67469 >>logs/bangkok_zags.txt 2>>logs/bangkok_zags.err  &
+sleep 6
+nohup python3 -m monitor_queue "https://bangkok.kdmid.ru/queue/orderinfo.aspx?id=67472&cd=ef84a764&ems=823942AE" 67472 >>logs/bangkok_drv.txt 2>>logs/bangkok_drv.err  &
+sleep 6
+nohup python3 -m monitor_queue "https://bangkok.kdmid.ru/queue/orderinfo.aspx?id=67473&cd=4193547b&ems=64FD4F5A" 67473 >>logs/bangkok_fidelity.txt 2>>logs/bangkok_fidelity.err  &
+sleep 6
+nohup python3 -m monitor_queue "https://bangkok.kdmid.ru/queue/orderinfo.aspx?id=67476&cd=487c3362&ems=C4664A24" 67476 >>logs/bangkok_authencity.txt 2>>logs/bangkok_authencity.err  &
+sleep 6
+nohup python3 -m monitor_queue "https://bangkok.kdmid.ru/queue/orderinfo.aspx?id=67475&cd=5952092d&ems=DA854072" 67475 >>logs/bangkok_attorney.txt 2>>logs/bangkok_attorney.err  &
+sleep 6
+nohup python3 -m monitor_queue "https://bangkok.kdmid.ru/queue/orderinfo.aspx?id=67474&cd=298e183f&ems=DAD24AF0" 67474 >>logs/bangkok_other.txt 2>>logs/bangkok_other.err  &
+sleep 6
+
+
+
+
+#PHUKET
 #NOTARY/PHUKET
-nohup python3 -m monitor_queue "https://phuket.kdmid.ru/queue/orderinfo.aspx?id=1259&cd=83fc0856&ems=77564E7C" 1259 >>logs/phuket_notary.txt 2>>logs/phuket_notary.err &
-sleep 61
+nohup python3 -m monitor_queue "https://phuket.kdmid.ru/queue/orderinfo.aspx?id=1284&cd=9c838d60&ems=94504ED1" 1284 >>logs/phuket_notary.txt 2>>logs/phuket_notary.err &
+sleep 6
 #PASPORT/PHUKET
-nohup python3 -m monitor_queue "https://phuket.kdmid.ru/queue/orderinfo.aspx?id=1258&cd=baff6793&ems=0C47474F" 1258 >>logs/phuket_pasport.txt 2>>logs/phuket_pasport.err &
-sleep 61
+nohup python3 -m monitor_queue "https://phuket.kdmid.ru/queue/orderinfo.aspx?id=1283&cd=202d8241&ems=B76D4F0E" 1283 >>logs/phuket_pasport.txt 2>>logs/phuket_pasport.err &
+sleep 6
 nohup python3 -m monitor_queue "https://phuket.kdmid.ru/queue/orderinfo.aspx?id=765&cd=79fd11d2&ems=F5B642CE" 765  >>logs/phuket_conviction.txt 2>>logs/phuket_conviction.err &
-sleep 61
+sleep 6
 nohup python3 -m monitor_queue "https://phuket.kdmid.ru/queue/orderinfo.aspx?id=667&cd=13a8e90a&ems=19864095" 667  >>logs/phuket_legalization.txt 2>>logs/phuket_legalization.err &
-sleep 61
+sleep 6
 
 
 
-nohup python3 -m monitor_queue "https://bangkok.kdmid.ru/queue/orderinfo.aspx?id=66585&cd=ff6eaeda&ems=FA20485E" 66585 &
-sleep 61
-nohup python3 -m monitor_queue "https://bangkok.kdmid.ru/queue/orderinfo.aspx?id=66587&cd=3be13f2c&ems=E5084559" 66587 &
-sleep 61
-nohup python3 -m monitor_queue "https://bangkok.kdmid.ru/queue/orderinfo.aspx?id=66586&cd=b682c23e&ems=B4814514" 66586 &
-sleep 61
-nohup python3 -m monitor_queue "https://bangkok.kdmid.ru/queue/orderinfo.aspx?id=66588&cd=c0056fab&ems=B590445F" 66588 &
-sleep 61
-nohup python3 -m monitor_queue "https://bangkok.kdmid.ru/queue/orderinfo.aspx?id=66589&cd=dbc1a5f3&ems=F62F41E1" 66589 &
-sleep 61
-nohup python3 -m monitor_queue "https://bangkok.kdmid.ru/queue/orderinfo.aspx?id=66584&cd=7d380255&ems=1C384C9B" 66584 &
-sleep 61
-nohup python3 -m monitor_queue "https://bangkok.kdmid.ru/queue/orderinfo.aspx?id=66582&cd=3da54122&ems=4FA54340" 66582 &
-sleep 61
+
+
+
+exit 0
+
+
